00b75ba6b7b9c6480047324f7835fc46a191bb41
Pavan <mail.pavangeetha78@gmail.com>
2024-09-01 16:41:22 +0530

    Alteration according to function

--- code_.py
+++ code_.py
@@ -12,20 +12,24 @@ opt = st.sidebar.selectbox("Choose Image Type", [
                            "Landscape", "Person", "Animals", "Flowers"])
 opt = opt.lower()
 
-img_file = st.sidebar.file_uploader(
+img_file_input = st.sidebar.file_uploader(
     "Upload Image", type=["jpg", "jpeg", "png"])
+img_file_target= st.sidebar.file_uploader(
+    "Upload Image", type=["jpg", "jpeg", "png"])
+if img_file_input is not None and img_file_target is not None:
+    # Your code here
 
-if img_file is not None:
     try:
-        image = Image.open(BytesIO(img_file.read()))
-        H, W = image.size
-        image = image.resize((256, 256))
+        image_input = Image.open(BytesIO(img_file_input.read()))
+        image_target= Image.open(BytesIO(img_file_target.read()))
+        image_input = image_input.resize((256, 256))
+        image_target = image_target.resize((256, 256))
         btn = st.sidebar.button("Colorize")
         if btn:
-            out = # pass into the model
+            out = # pass the generate_images function to get the required output
             col1, col2 = st.columns(2)
             with col1:
-                st.image(image, use_column_width=True, caption="Original")
+                st.image(image_target, use_column_width=True, caption="Original")
             with col2:
                 st.image(out, use_column_width=True, caption="Coloured")
     except Exception as e:
